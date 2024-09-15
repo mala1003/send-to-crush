e85420b85fd2be195c8b449b20c089097cfaabfc
mala1003 <mala1003mala1003@gmail.com>
2024-09-15 13:06:33 +0100

    script.js

--- send-to-crush/script.js
+++ send-to-crush/script.js
@@ -5,8 +5,9 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "You got Rickrolled 😘 (FOLLOW ME @kristel_tech or 7 years bad luck)";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  question.innerHTML = "htni nhbkkkkk yee rouhyyy w hyetii w 3ineye w 3omrii w mleyktii w princestiii w mrytii w gamrtii w chmsiii ett batmantiii w deloultiii w blwtiiii w hayewniii l2eliffff yee 9atoustiii w 8zeltiii w arnoubtiiii yee rouhh filkk yee frhtiii w dhktnii w most9bliii w dweye w nawartiii w nejmtiii nmouttt 3likk yee li mhbltnii w m5dhtlii 39lii w 9lbii w kol chy fiyee yee om wledii e7em hhh yee chrekitt 7yetii yee lbara2aa w zinn bkolou yee 2mll mt3iii lwhid na3ch9kk yee hobii w lhob bkolou yee 7olmii yee ahlee haje sartlii fi hyetiii yee kenzii w n3mtii w nourrr dnytiii yee 9lbiii w a8lee mee 3ndii ett mt3ii whdii yee fatoumtiii
+ 😘 ";
+  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExanU0cnF2MWNlYXJ2cWtkdjNxdnZzeHZrcmV4d2sxMXdlNmUxNTZ3MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/c76IJLufpNwSULPk77/giphy.gif";
 });
 
 // Make the No button move randomly on hover
